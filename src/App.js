b60f4c715782f4a00cd10731caa2746f4557734b
Tamim <tamim-hossain>
2020-11-19 13:02:27 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import Achievements from './components/Achievements/Achievements';
 import Contact from './components/Contact/Contact';
 import Email from './components/Email/Email';
 import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Menu from './components/Menu/Menu';
 import Plans from './components/Plans/Plans';
 import Services from './components/Services/Services';
+import Main from './components/Main/Main';
 
 const App = () => {
     return (
         <>
-            <Menu />
-            <Header />
+            <Main />
             <Services />
             <Contact />
             <Achievements />
---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Menu from '../Menu/Menu';
+
+const Main = () => {
+    return (
+        <header>
+            <Menu />
+            <Header />
+        </header>
+    );
+};
+
+export default Main;
